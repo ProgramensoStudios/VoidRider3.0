e1a5633c75b68709d7fef1a059bbedde25bd677b
Charly <crono_zs@hotmail.com>
2024-11-09 03:58:24 -0600

    práparando para hacer la funcionalidad de damage al player

---
+++ VoidRiderr/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private int _health = 20;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+    }
+}
